966a2c4b59aeb68ecb49708bbce7185846a29727
Wilson-Epitek <wilson.wu@epitech.digital>
2025-09-25 15:38:36 +0200

    Page d'accueil

---
+++ test_app/screens/HomeScreen.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('../assets/toilette.png')}
+          style={styles.logo}
+        />
+      </View>
+
+     
+      <Text style={styles.title}>Bienvenue sur PoopStyle</Text>
+
+      
+      <Text style={styles.subtitle}>
+        La seule et unique application qui te permet de géolocaliser les
+        toilettes publiques dans Paris !
+      </Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Map')}
+      >
+        <Text style={styles.buttonText}>Continuer</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#d6e9d4', // fond vert clair
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  logoContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 20,
+    marginBottom: 30,
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    color: '#333',
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: 15,
+    color: '#555',
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+  button: {
+    backgroundColor: '#a4d46c',
+    paddingVertical: 15,
+    paddingHorizontal: 60,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '600',
+    fontSize: 16,
+  },
+});
