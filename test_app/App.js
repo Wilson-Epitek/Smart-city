cc1d12d8895d36e4f7eed0042d73c7d4dde75c82
QueenMae <maena.brice@epitech.digital>
2025-09-26 17:29:25 +0200

    Version finale

--- test_app/App.js
+++ test_app/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+
 import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
+
 
 const Tab = createBottomTabNavigator();
 
